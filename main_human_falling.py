639118ee1b178ac60b5e68e64e5e73d3c24619ef
suphakrit <suphakrit_kongkham@cmu.ac.th>
2023-08-09 00:31:30 +0700

    new comment

--- main_human_falling.py
+++ main_human_falling.py
@@ -6,32 +6,34 @@ import numpy                        # This library provides support for multi-di
 import requests                     # This library is used to send HTTP requests in Python.
 from pydantic import BaseModel      # This is a base class for creating models
 import math                             
-import pandas as pd
-import datetime
-import time
-import pytz
-import smtplib
+import pandas as pd                 # Organizing data library
+import datetime                     # Time library                   
+import pytz                         # Time-zone library
+import smtplib                      # APIs of email communication
 from email.mime.multipart import MIMEMultipart
 from email.mime.base import MIMEBase
 from email.mime.text import MIMEText
 from email import encoders
-import paho.mqtt.client as mqtt
+import paho.mqtt.client as mqtt     # APIs of mqtt
 
-
+#configurations for MQTT
 MQTT_BROKER_HOST = "192.168.1.107"
 MQTT_BROKER_PORT = 1883
 MQTT_TOPIC = "/home/falling"
 MQTT_USERNAME = "mqttbroker"
 MQTT_PASSWORD = "12345678"
 
+#setup MQTT
 mqtt_client = mqtt.Client()
 mqtt_client.username_pw_set(MQTT_USERNAME, MQTT_PASSWORD)
 mqtt_client.connect(MQTT_BROKER_HOST, MQTT_BROKER_PORT)
 
+#setup REAL-TIME
 desired_timezone = pytz.timezone('Asia/Bangkok')
 current_time = datetime.datetime.now(desired_timezone)
 excel_file = ("data_{}.xlsx".format(current_time.date()))
 
+########   attach .xlsx file via email function  #######
 def send_email_with_attachment(xlsx_path):
     # Set up email parameters
     smtp_server = 'smtp.gmail.com'  # Replace with your email server
@@ -39,9 +41,9 @@ def send_email_with_attachment(xlsx_path):
 
     subject = "Almshouse Project"
     body = "Hello, please find the attached .xlsx file."
-    from_email = "forproject0858@gmail.com"
-    from_password = "kdrkckkofjapeddm"
-    to_email = "supakritgongkam@gmail.com"
+    from_email = "forproject0858@gmail.com"     #host email
+    from_password = "kdrkckkofjapeddm9"          #token 
+    to_email = "destiny email"
     
     # Create the message container
     msg = MIMEMultipart()
@@ -67,10 +69,11 @@ def send_email_with_attachment(xlsx_path):
     server.sendmail(from_email, to_email, msg.as_string())
     server.quit()
 
-
+########   Set ending time for sent summary data #######
 def endofday(current_time):
     return (current_time.hour == 23 and current_time.minute == 58) and current_time.weekday()==6
 
+########   Clear .xlsx file function  ########
 def remove_excel_file(file_path):
     try:
         if os.path.exists(file_path):
@@ -79,6 +82,8 @@ def remove_excel_file(file_path):
     except Exception as e:
         print(f"Error: {e}")
 
+
+#######  Write messages to exel function  ######## 
 def save_exel(message):
 
     data ={"Event":[message],"Date":[str(current_time.date())], "Time":[str(current_time.strftime("%H:%M:%S"))]}
@@ -98,7 +103,7 @@ def save_exel(message):
         writer._save()
 
 
-#This code below is the LINE Notificate function.
+#######  This code below is the LINE Notificate function.
 def send_line_notify(message, image_path):
     url = "https://notify-api.line.me/api/notify"
     token = "PxOmKihu9ghjvtd68yguoiohihpujopj999"  # Replace with your Line Notify token
@@ -111,7 +116,7 @@ def send_line_notify(message, image_path):
     files = {"imageFile": open(image_path, "rb")}
     r = requests.post(url, headers=headers, data=payload, files=files)
 
-#This function help to identify each of keypoints.
+####  This function help to identify each of keypoints.
 class GetKeypoint(BaseModel):
     NOSE:           int = 0
     LEFT_EYE:       int = 1
@@ -188,15 +193,20 @@ while cap.isOpened():
                     right_ankle_x, right_ankle_y = result_keypoint[i][get_keypoint.RIGHT_ANKLE]
                     left_ankle_x, left_ankle_y = result_keypoint[i][get_keypoint.LEFT_ANKLE]
                     
-                    if status == True:
+                    
+                    
+                    if status == True:                                  # status condition for sleeping process
                         if frame_time2 % delay_frame_stg == 0  :
                             scale.clear()
                             frame_time2 = 1
                         else :
                             frame_time2 = frame_time2 +1
-                        if left_hip_y and right_hip_y < 1 :
+                        if left_hip_y and right_hip_y < 1 :         # if hip are in frame then continue the process
+                            
+                            #### This part is computed with math  #######
+                            #### Using Vector, Dot product, and projection of vector #####
+                            
                             center_ear = [(left_ear_x+right_ear_x)/2 , (left_ear_y+right_ear_y)/2]
-                            #center_shd = [(left_shd_x+right_shd_x)/2 , (left_shd_y+right_shd_y)/2]
                             center_hip = [(left_hip_x+right_hip_x)/2 , (left_hip_y+right_hip_y)/2]
                             center_ankle = [(left_ankle_x+right_ankle_x)/2 , (left_ankle_y+right_ankle_y)/2]
                             vector_l1 = [(center_ear[0] - center_hip[0]), (center_ear[1] - center_hip[1])]
@@ -218,15 +228,14 @@ while cap.isOpened():
                             cv2.putText(annotations_frame, str(round(angle,2)) , (480, 40*(i+1)), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 0), 2)
                             cv2.putText(annotations_frame, (" STATUS: {} ".format(status)), (380, 450), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 0), 2)
 
-                        ####   thess code below are conditions ####
-                        
+                            ### Calculate the speed per frame ###
                             scale.append(angle)   
                             speed = abs((scale[len(scale)-1]-scale[0])/frame_time2)
                             cv2.putText(annotations_frame, ("Speed[{}]: ".format(i+1)) , (40, 40*(i+1)), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 0), 2)
                             cv2.putText(annotations_frame, str(round(speed,2)) , (200, 40*(i+1)), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 0), 2)
 
 
-                              
+                            ####   thess code below are conditions ####
 
                             if abs(right_hip_x - right_shd_x) > abs(right_hip_y - right_shd_y):
                                 print("falling............")
@@ -269,11 +278,14 @@ while cap.isOpened():
                             status_delay = 0
                             status_count = status_count +1
 
-    if status_count == 4:
+     # sent exel file to email every 4 event happend
+    if status_count == 4:  
         xlsx_path = excel_file
         send_email_with_attachment(xlsx_path)
         status_count = 0
 
+
+    # sent summary file every week
     current_time = datetime.datetime.now(desired_timezone)
     if endofday(current_time):
         xlsx_path = excel_file
